298425faf4b562f3f1048fe1987e9bddb619d795
huangguoqiang <guoqiang.huang@pdxchg.com>
2017-05-25 18:02:37 +0800

    讯飞在线语音合成官方Demo优化

--- app/src/main/java/demo/captain/xfspeech/MainActivity.java
+++ app/src/main/java/demo/captain/xfspeech/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import demo.captain.xfspeech.xfutils.SpeechSynthesizerUtil;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
